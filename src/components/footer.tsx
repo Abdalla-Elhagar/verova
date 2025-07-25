810b0555e4e077786cdd27168bcf002b55ec9154
Abdalla Elhagar <nanoelhgar@gmail.com>
2025-07-25 21:43:13 +0300

    Refactor footer component to use semantic HTML elements
    - Changed contact, social media, company, and terms sections from divs to unordered lists for better accessibility and structure.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -71,41 +71,41 @@ export default function Footer() {
             <div className="top flex justify-center lg:justify-between items-start gap-20 md:text-start text-center  flex-wrap ">
                 <div className="contact">
                     <h3 className='text-2xl font-semibold mb-3'>Contact</h3>
-                    <div className='flex flex-col items-start gap-1'>
+                    <ul className='flex flex-col items-start gap-1'>
                         {contactLinks.map((l,index)=> (
-                            <div className="w-full" key={index}>{l.title}</div>
+                            <li className="w-full" key={index}>{l.title}</li>
                         ))}
-                    </div>
+                    </ul>
                 </div>
                 
 
                 <div className="Social">
                     <h3 className='text-2xl font-semibold mb-3'>Social Media</h3>
-                    <div className='flex flex-col items-end gap-1'>
+                    <ul className='flex flex-col items-end gap-1'>
                         {socialMediaLinks.map((l,index)=> (
-                            <div className="w-full" key={index}>{l.title}</div>
+                            <li className="w-full" key={index}>{l.title}</li>
                         ))}
-                    </div>
+                    </ul>
                 </div>
                 
 
                 <div className="Company">
                     <h3 className='text-2xl font-semibold mb-3'>Company</h3>
-                    <div className='flex flex-col items-end gap-1'>
+                    <ul className='flex flex-col items-end gap-1'>
                         {companyLinks.map((l,index)=> (
-                            <div className="w-full" key={index}>{l.title}</div>
+                            <li className="w-full" key={index}>{l.title}</li>
                         ))}
-                    </div>
+                    </ul>
                 </div>
                 
 
                 <div className="Terms">
                     <h3 className='text-2xl font-semibold mb-3'>Terms & Policy</h3>
-                    <div className='flex flex-col items-end gap-1'>
+                    <ul className='flex flex-col items-end gap-1'>
                         {termsAndPolicy.map((l,index)=> (
-                            <div className="w-full" key={index}>{l.title}</div>
+                            <li className="w-full" key={index}>{l.title}</li>
                         ))}
-                    </div>
+                    </ul>
                 </div>
                 
 
