ca19b3d8c29c87702c9de0046cd039d5c67e8531
ahmedwaelalkaliby <ahmedalkaliby@gmail.com>
2025-08-10 06:20:14 +0300

    men's and women's pages

--- src/app/women/page.tsx
+++ src/app/women/page.tsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React from 'react';
+import ProductCard from '@/components/productCard';
+import  fetchAllProducts  from '@/utils/allProducts';
 
-export default function Women() {
+
+  export interface Product {
+  id: number;
+  imageCover: string;
+  title: string;
+  description: string;
+  price: number;
+  category: {
+    name: string;
+  };
+}
+
+let products: Product[] = [];
+
+export default async function Shop() {
+  
+
+  try {
+    products = await fetchAllProducts();
+  } catch (error) {
+    console.error('Error fetching products:', error);
+  }
+
   return (
-    <div>women</div>
-  )
+    <>
+      <div className="grid grid-cols-1 sm:grid-cols-3 justify-items-center">
+      {
+        products.length> 0 ? products.filter((product) => product.category.name === "Women's Fashion").map((product) =>
+            <ProductCard key={product.id} product={product} />
+         ) :<div>No products found.</div>
+      }
+     </div>
+    </>
+  );
 }
